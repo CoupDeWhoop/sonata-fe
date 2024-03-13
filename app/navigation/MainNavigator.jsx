ab0596fe5cf115b29a18580d5c60c7d5bf27c0ff
CoupDeWhoop <greenland.david@gmail.com>
2024-03-13 10:01:38 +0000

    add tab bar icons

--- app/navigation/MainNavigator.jsx
+++ app/navigation/MainNavigator.jsx
@@ -1,6 +1,7 @@
 import HomeScreen from '../screens/HomeScreen.jsx';
 import PracticeScreen from '../screens/PracticeScreen.jsx';
 import StatsScreen from '../screens/StatsScreen.jsx';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
@@ -9,10 +10,32 @@ const Tab = createBottomTabNavigator();
 
 export const MainNavigator = () => (
     <Tab.Navigator screenOptions={{
-        headerShown: false
-        }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Practice Sessions" component={PracticeScreen} />
-        <Tab.Screen name="Stats" component={StatsScreen}/>
+        headerShown: false,
+        tabBarLabelStyle:{
+            fontSize: 14
+        }
+        }} 
+        >
+        <Tab.Screen name="Home" component={HomeScreen}         
+        options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+        }}/>
+        <Tab.Screen name="Practice Sessions" component={PracticeScreen}
+                options={{
+            tabBarLabel: 'Practice',
+            tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="piano" color={color} size={26} />
+            ),
+        }} />
+        <Tab.Screen name="Stats" component={StatsScreen}
+                options={{
+            tabBarLabel: 'Stats',
+            tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="chart-bar" color={color} size={26} />
+            ),
+            }} />
     </Tab.Navigator>
 );
