2eb44d0255baea0f1a30d539fea14f742c940698
CoupDeWhoop <greenland.david@gmail.com>
2024-03-19 10:04:13 +0000

    adds error snackbar to LessonNotesScreen

--- app/screens/LessonNotesScreen.jsx
+++ app/screens/LessonNotesScreen.jsx
@@ -1,11 +1,11 @@
 import { useState, useRef } from 'react';
 import { View, StyleSheet, FlatList, ScrollView } from "react-native"
-import { Button, Card, MD2Colors, Text, TextInput } from "react-native-paper"
-import { postNote } from '../utils';
+import { Button, Card, MD2Colors, Snackbar, Text, TextInput } from "react-native-paper"
+import { formatDate, formatTime, postNote } from '../utils';
 
 
 export default LessonNotesScreen = ({lesson, setNewLesson}) => {
-    const [msgVisible, setMsgVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
     const [learningFocus, setLearningFocus] = useState('');
     const [notes, setNotes] = useState('');
     const [error, setError] = useState(null)
@@ -14,10 +14,11 @@ export default LessonNotesScreen = ({lesson, setNewLesson}) => {
     const learningFocusInputRef = useRef(null);
     const notesInputRef = useRef(null);
 
+
     const handleSubmitNote = async() => {
         if (!learningFocus || !notes) {
             setError("Learning Focus and Notes are required");
-            console.error(error)
+            setVisible(true)
             return;
         }
 
@@ -47,58 +48,69 @@ export default LessonNotesScreen = ({lesson, setNewLesson}) => {
 
     return (
         <View style={{flex: 1, padding: 16, paddingBottom: 8}}>
-                <Text variant="headlineLarge">{`${lesson.lesson_id}`}</Text>
-        <FlatList
-            contentContainerStyle={{ marginTop: 16, paddingBottom: 4}}
-            data={lesson.notes}
-            keyExtractor={(item) => item.note_id}
-            renderItem={( {item }) => (
-                <View>
-                    <Card style={styles.card}>
-                        <Card.Title
-                            title={item.learning_focus}
-                        />
-                        <Card.Content>
-                            <Text variant="bodyMedium">{item.notes}</Text>
-                        </Card.Content>
-                    </Card>
-                </View>
-            )}
-        />
-                <TextInput 
-                    ref={learningFocusInputRef} 
-                    style={styles.learningFocusInput} 
-                    mode={'outlined'} 
-                    label={Label('Learning Focus')}
-                    value={learningFocus}
-                    onChangeText={text => setLearningFocus(text)}
-                />
-                <TextInput 
-                    ref={notesInputRef} 
-                    style={styles.noteInput} 
-                    mode={'outlined'} 
-                    multiline={true}
-                    placeholder='Lesson Notes'
-                    placeholderTextColor={MD2Colors.grey500}
-                    value={notes}
-                    onChangeText={text => setNotes(text)}
-                />
-                <Button 
-                    mode="contained" 
-                    style={styles.submitButton}
-                    buttonColor={MD2Colors.pink200} 
-                    labelStyle={{fontSize: 14}} 
-                    disabled={isSubmitting}
-                    onPress={handleSubmitNote}>
-                    Add Note
-                </Button>
- 
-
+            <Text style={styles.title} variant="titleLarge">{`${formatDate(lesson.lesson_timestamp, 'long')} - ${formatTime(lesson.lesson_timestamp)}`}</Text>
+            <FlatList
+                contentContainerStyle={{ marginTop: 16, paddingBottom: 4}}
+                data={lesson.notes}
+                keyExtractor={(item) => item.note_id}
+                renderItem={( {item }) => (
+                    <View>
+                        <Card style={styles.card}>
+                            <Card.Title
+                                title={item.learning_focus}
+                            />
+                            <Card.Content>
+                                <Text variant="bodyMedium">{item.notes}</Text>
+                            </Card.Content>
+                        </Card>
+                    </View>
+                )}
+            />
+            <TextInput 
+                ref={learningFocusInputRef} 
+                style={styles.learningFocusInput} 
+                mode={'outlined'} 
+                label={Label('Learning Focus')}
+                value={learningFocus}
+                onChangeText={text => setLearningFocus(text)}
+            />
+            <TextInput 
+                ref={notesInputRef} 
+                style={styles.noteInput} 
+                mode={'outlined'} 
+                multiline={true}
+                placeholder='Lesson Notes'
+                placeholderTextColor={MD2Colors.grey500}
+                value={notes}
+                onChangeText={text => setNotes(text)}
+            />
+            <Button 
+                mode="contained" 
+                style={styles.submitButton}
+                buttonColor={MD2Colors.pink200} 
+                labelStyle={{fontSize: 14}} 
+                disabled={isSubmitting}
+                onPress={handleSubmitNote}>
+                Add Note
+            </Button>
+            <View>
+                <Snackbar
+                    visible={visible}
+                    onDismiss={() => setVisible(false)}
+                    action={{
+                        label: 'Close'
+                    }}>
+                    Learning Focus and Notes are required
+                </Snackbar>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    title: {
+        textAlign: 'center'
+    },
     card: {
         margin: 2,
         marginBottom:4,
