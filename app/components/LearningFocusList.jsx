48314698df9f1e8929d7b5a494214430e1802a4e
CoupDeWhoop <greenland.david@gmail.com>
2024-03-25 12:49:07 +0000

    updated learning focus list

--- app/components/LearningFocusList.jsx
+++ app/components/LearningFocusList.jsx
@@ -1,26 +1,59 @@
 import * as react from 'react';
 import { FlatList } from 'react-native';
 import { Card, Text } from 'react-native-paper';
+import { AppContext } from '../context/AppProvider';
+import { formatDate } from '../utils';
 
 
-export const LearningFocusList = ({lessons, setLearningFocus}) => (
-    <FlatList 
-        style={{flexGrow: 0, flexShrink: 0}}
-        horizontal={true}
-        data={lessons}
-        keyExtractor={(item) => {
-            return item.lesson_id
-        }}
-        renderItem={( {item }) => item.notes.map((note) => (
-            <Card
-                key={note.note_id} 
+export const LearningFocusList = () => {
+
+    const { lessons, practises } = react.useContext(AppContext);
+
+    const allData = [...lessons, ...practises];
+
+    const organizedData = {};
+    
+    allData.forEach(item => {
+        if (item.notes) {
+            item.notes.forEach(note => {
+                const timestamp = item.lesson_timestamp || item.practice_timestamp;
+                if (!organizedData[note.learning_focus]) {
+                    organizedData[note.learning_focus] = [];
+                }
+                organizedData[note.learning_focus].push({
+                    type: item.lesson_timestamp ? 'lesson' : 'practice',
+                    id: item.lesson_id || item.practice_id,
+                    timestamp: timestamp,
+                    duration: item.duration,
+                    noteId: note.note_id,
+                    noteContent: note.note_content
+                });
+            });
+        }
+
+    });
+    
+    const data = Object.keys(organizedData);
+
+    return (
+        <FlatList 
+            style={{flexGrow: 0, flexShrink: 0}}
+            horizontal={true}
+            data={data}
+            keyExtractor={(item, index) => {
+                return index
+            }}
+            renderItem={({item}) => (
+                <Card
                 style={{margin: 2}} 
-                onPress={() => setLearningFocus(note.learning_focus)}
-            >
-                <Card.Content>
-                <Text variant="labelLarge" >{note.learning_focus}</Text>
-                </Card.Content>
-            </Card>
-        ))}
-    />
-)
+                // onPress={() => setLearningFocus(note.learning_focus)}
+                >
+                    <Card.Content>
+                    <Text variant="labelLarge">{item}</Text>
+                    <Text variant="labelMedium">{formatDate(organizedData[item][0].timestamp)}</Text>
+                    </Card.Content>
+                </Card>
+            )}
+        />
+    )
+}
--- app/screens/PracticeScreen.jsx
+++ app/screens/PracticeScreen.jsx
@@ -10,6 +10,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/AntDesign.js';
 import { PracticeModalContext } from '../context/PracticeModalContext';
 import { AppContext } from '../context/AppProvider';
+import { LearningFocusList } from '../components/LearningFocusList';
 
 export default PracticeScreen = () => {
     const { practises, loading } = useContext(AppContext);
@@ -20,34 +21,41 @@ export default PracticeScreen = () => {
     let previousDate = null;
 
     return (
-      <SafeAreaView style={commonStyles.layout}>
-          <ScrollView>
-              {practises.map((practice, index) => {
-                  const currentDate = new Date(practice.practice_timestamp).toDateString();
-                  const showDate = currentDate !== previousDate; // Check if it's a new date
+        <SafeAreaView style={commonStyles.layout}>
+            <Title>Last Practice</Title>
+            <Text>{formatDate(practises[practises.length -1].practice_timestamp)}</Text>
+            <Title>Recent learning focus</Title>
+            <LearningFocusList />
+            <ScrollView horizontal={true}>
 
-                  previousDate = currentDate; // Update previousDate
-
-                  return (
-                      <View style={styles.day} key={index}>
-                          {showDate ? <Text variant="titleMedium" style={styles.date}>{formatDate(practice.practice_timestamp)}</Text> : null}
-                          <PracticeCard
-                              notes={practice.notes}
-                              timestamp={practice.practice_timestamp}
-                              duration={practice.duration}
-                          />
-                      </View>
-                  );
-              })}
-                <View>
-                    <Title>Calendar</Title>
-                    <PracticeCalendar practises={practises}/>
-                </View>
             </ScrollView>
-            <TouchableOpacity style={styles.addButton} >
-              <Icon name="pluscircle" size={54} color="tomato" onPress={() => setPracticeModalIsVisible(true)}/>
-            </TouchableOpacity>
-        </SafeAreaView>
+            <ScrollView>
+                {practises.map((practice, index) => {
+                    const currentDate = new Date(practice.practice_timestamp).toDateString();
+                    const showDate = currentDate !== previousDate; // Check if it's a new date
+
+                    previousDate = currentDate; // Update previousDate
+
+                    return (
+                        <View style={styles.day} key={index}>
+                            {showDate ? <Text variant="titleMedium" style={styles.date}>{formatDate(practice.practice_timestamp)}</Text> : null}
+                            <PracticeCard
+                                notes={practice.notes}
+                                timestamp={practice.practice_timestamp}
+                                duration={practice.duration}
+                            />
+                        </View>
+                    );
+                })}
+                    <View>
+                        <Title>Calendar</Title>
+                        <PracticeCalendar practises={practises}/>
+                    </View>
+                </ScrollView>
+                <TouchableOpacity style={styles.addButton} >
+                <Icon name="pluscircle" size={54} color="tomato" onPress={() => setPracticeModalIsVisible(true)}/>
+                </TouchableOpacity>
+            </SafeAreaView>
 
     )
 }
